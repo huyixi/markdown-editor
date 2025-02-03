29b982ab2fb76ec5098063fee60a857cc8bdca97
huyixi <hi@huyixi.com>
2025-02-03 18:13:41 +0800

    chore

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-import MarkdownIt from "markdown-it";
+import markdownit from "markdown-it";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), MarkdownIt()],
+  plugins: [vue(), tailwindcss(), markdownit()],
 });
